5135050a93c2e79800b2e0910419c1f8902975af
steden <1470804@qq.com>
2025-05-11 21:24:11 +0800

    监听容器事件

--- event.go
+++ event.go
@@ -10,7 +10,7 @@ type event struct {
 }
 
 // Watch 持续获取docker事件
-func (receiver event) Watch(image string) chan EventResult {
+func (receiver event) Watch() chan EventResult {
 	eventResultChan := make(chan EventResult, 1000)
 	// 将读取到的json事件信息转换成EventResult结构体
 	go func() {
@@ -29,25 +29,25 @@ func (receiver event) Watch(image string) chan EventResult {
 
 // docker 事件结构体
 type EventResult struct {
-	Status string `json:"status"`
-	ID     string `json:"id"`
-	From   string `json:"from"`
-	Type   string `json:"Type"`
-	Action string `json:"Action"`
+	Status string `json:"status"` // 事件类型
+	ID     string `json:"id"`     // 容器ID
+	From   string `json:"from"`   // 镜像
+	Type   string `json:"Type"`   // container
+	Action string `json:"Action"` // start
 	Actor  struct {
-		ID         string `json:"ID"`
+		ID         string `json:"ID"` // 容器ID
 		Attributes struct {
-			ComDockerSwarmNodeID      string `json:"com.docker.swarm.node.id"`
-			ComDockerSwarmServiceID   string `json:"com.docker.swarm.service.id"`
-			ComDockerSwarmServiceName string `json:"com.docker.swarm.service.name"`
+			ComDockerSwarmNodeID      string `json:"com.docker.swarm.node.id"`      // swarm节点ID
+			ComDockerSwarmServiceID   string `json:"com.docker.swarm.service.id"`   // swarm服务ID
+			ComDockerSwarmServiceName string `json:"com.docker.swarm.service.name"` // swarm服务名称（容器名称）
 			ComDockerSwarmTask        string `json:"com.docker.swarm.task"`
 			ComDockerSwarmTaskID      string `json:"com.docker.swarm.task.id"`
-			ComDockerSwarmTaskName    string `json:"com.docker.swarm.task.name"`
-			Image                     string `json:"image"`
-			Name                      string `json:"name"`
+			ComDockerSwarmTaskName    string `json:"com.docker.swarm.task.name"` // 节点所在的实例名称
+			Image                     string `json:"image"`                      // 镜像
+			Name                      string `json:"name"`                       // 节点所在的实例名称
 		} `json:"Attributes"`
 	} `json:"Actor"`
 	Scope    string `json:"scope"`
-	Time     int    `json:"time"`
-	TimeNano int64  `json:"timeNano"`
+	Time     int    `json:"time"`     // 发生时间（秒）
+	TimeNano int64  `json:"timeNano"` // 发生时间（纳秒）
 }
